897c48dbc3435630e062611257d7b8e950aecdbd
Vinayak pandey <vinayakpandey286@gmail.com>
2024-10-25 07:23:12 +0530

    adding hero and appabar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Appbar from "@/components/Appbar";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
-      <div></div>
+      <Appbar />
+      <Hero />
     </>
   );
 }
